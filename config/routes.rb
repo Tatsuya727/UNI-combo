6b0713d24403d2c6cb8e5c0242dfdd1baa32a504
tatsuya <babatsuya@icloud.com>
2023-03-27 13:26:00 +0900

    make activation_digest

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  root "posts#index"
-  get  "/signup", to: "users#new" 
-  get '/login',   to: "sessions#new"
-  post '/login',   to: "sessions#create"
-  delete '/logout',   to: "sessions#destroy"
+  root   "posts#index"
+  get    "/signup",   to: "users#new" 
+  get    "/login",    to: "sessions#new"
+  post   "/login",    to: "sessions#create"
+  delete "/logout",   to: "sessions#destroy"
   resources :users
+  resources :account_activations, only: [:edit]
 end
