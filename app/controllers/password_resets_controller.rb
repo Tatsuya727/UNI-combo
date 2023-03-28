73227edbb57a511d8f9548d1b776511e6936c45d
tatsuya <babatsuya@icloud.com>
2023-03-28 21:23:47 +0900

    パスワードリセット（途中）

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,20 @@
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "入力されたメールアドレスに送られたリンクを確認してください。"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "このメールアドレスは登録されていません"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+end
