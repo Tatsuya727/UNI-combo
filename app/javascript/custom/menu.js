f0700882626e217870d77068011c281049fbd6ec
tatsuya <babatsuya@icloud.com>
2023-04-30 17:23:27 +0900

    フィルター機能

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -18,3 +18,50 @@ document.addEventListener("turbo:load", function () {
 
 
 // キャラクタータグのサイドメニュー
+function filterCommands(prefixes) {
+  const listItems = document.querySelectorAll('.microposts li');
+  listItems.forEach(item => {
+    const commandElement = item.querySelector('.comando');
+    const commandText = commandElement.textContent;
+    const commands = commandText.split('>');
+    const hasMatchingPrefix = commands.some(command => prefixes.includes(command));
+    if (hasMatchingPrefix) {
+      item.style.display = 'list-item';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
+document.addEventListener('turbo:load', function() {
+  const applyFiltersButton = document.getElementById('apply-filters');
+  applyFiltersButton.addEventListener('click', function() {
+    const prefixFilterCheckboxes = document.querySelectorAll('.prefix-filter');
+    const checkedPrefixes = Array.from(prefixFilterCheckboxes)
+      .filter(checkbox => checkbox.checked)
+      .map(checkbox => checkbox.value);
+    filterCommands(checkedPrefixes);
+  });
+});
+
+document.addEventListener('turbo:load', function() {
+  const openModalButton = document.getElementById('open-modal');
+  const closeModalButton = document.getElementById('close-modal');
+  const filterModal = document.getElementById('filter-modal');
+
+  openModalButton.addEventListener('click', function() {
+    filterModal.style.display = 'block';
+  });
+
+  closeModalButton.addEventListener('click', function() {
+    filterModal.style.display = 'none';
+  });
+
+  window.addEventListener('click', function(event) {
+    if (event.target === filterModal) {
+      filterModal.style.display = 'none';
+    }
+  });
+});
+
+
