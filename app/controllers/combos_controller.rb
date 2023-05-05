3610802c80cd73b5c46f8fc2e0773d75774e0f9c
tatsuya <babatsuya@icloud.com>
2023-05-05 16:42:37 +0900

    edit user card

--- app/controllers/combos_controller.rb
+++ app/controllers/combos_controller.rb
@@ -1,5 +1,5 @@
 class CombosController < ApplicationController
-    before_action :logged_in_user, only: [:create, :destroy]
+    before_action :logged_in_user, only: [:create, :destroy, :new]
     before_action :correct_user,   only: [:destroy]
 
     def index
