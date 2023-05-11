a53291991e735c978233e897b839ee8e31b067dd
tatsuya <babatsuya@icloud.com>
2023-05-11 23:32:39 +0900

    投稿詳細ページのいいね

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,5 @@
 class LikesController < ApplicationController
     def create
-        puts 1
         @like= current_user.likes.create(combo_id: params[:combo_id])
         redirect_back(fallback_location: root_path)
     end
@@ -9,10 +8,5 @@ class LikesController < ApplicationController
         @like = Like.find_by(combo_id: params[:combo_id], user_id: current_user.id)
         @like.destroy
         redirect_back(fallback_location: root_path)
-    end
-
-    private
-    def like_params
-        params.require(:like).permit(:combo_id)
     end
 end
