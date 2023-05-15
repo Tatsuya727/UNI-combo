00a28482ab164dac9d4c1665b0a160a14df31553
tatsuya <babatsuya@icloud.com>
2023-05-15 12:28:32 +0900

    コメント投稿機能完成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
 
     def create
-        @combo   = Combo.find(params[:combo_id])
-        @comment = @combo.comments.build(comment_params)
+        @combo        = Combo.find(params[:combo_id])
+        @comment      = @combo.comments.build(comment_params)
+        @comment.user = current_user
         if @comment.save
-            redirect_to @combo
+            flash[:success] = "コメントを投稿しました"
+            redirect_to combo_path(@combo)
         else
-            redirect_to @combo
+            flash[:danger] = "コメントの投稿に失敗しました"
+            redirect_to combo_path(@combo)
         end
     end
 
     private
 
     def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:body)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_015500) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_15_031118) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,7 +66,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_015500) do
     t.integer "combo_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
     t.index ["combo_id"], name: "index_comments_on_combo_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -104,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_015500) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "combos", "users"
   add_foreign_key "comments", "combos"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "combos", on_delete: :cascade
   add_foreign_key "likes", "users"
 end
