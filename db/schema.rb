0412a9f5a75a6c2a4d8b385ce4a2949ac32a95a7
tatsuya <babatsuya@icloud.com>
2023-05-18 18:53:21 +0900

    ファイルの場所の変更に伴いパスの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_15_031118) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_18_094243) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -99,6 +99,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_15_031118) do
     t.datetime "activated_at"
     t.string "reset_digest"
     t.datetime "reset_sent_at"
+    t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
