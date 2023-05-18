a549b2894d780d5189046dd72d23fa46e15b0a75
tatsuya <babatsuya@icloud.com>
2023-05-18 21:54:58 +0900

    ddtrace install

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,6 @@
 Datadog.configure do |c|
-    c.use :rails, service_name: 'UNI-combo', database_service: 'postgresql'
-    c.tracer hostname: 'localhost', port: 8126
-    c.api_key = ENV['DATADOG_API_KEY']
+    c.use :rails, service_name: 'UNI-combo', log_injection: true
+    c.use :active_record, orm_service_name: 'my-rails-app-postgres'
+    c.tracer env: Rails.env, tags: { 'env' => Rails.env }
+    c.tracer.enabled = true
 end
