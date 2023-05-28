41f12a79797a58d28683a09842b0dc386119d3be
tatsuya <babatsuya@icloud.com>
2023-05-29 01:56:23 +0900

    ログインを失敗したときの処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       if user.activated? # メアドに送られたリンクをクリックすると
-        forwarding_url = session[:forwarding_url] # アクセスしようとしたURLを保存
-        reset_session
-        params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-        log_in user
-        redirect_to forwarding_url || user
+        if user.login_attempts < 7 || Time.zone.now - user.last_attemts_at > 1.hour # 7回以上ログイン失敗したら1時間後にリセット
+          user.update(login_attempts: 0, last_attemts_at: nil)
+          forwarding_url = session[:forwarding_url] # アクセスしようとしたURLを保存
+          reset_session
+          params[:session][:remember_me] == "1" ? remember(user) : forget(user)
+          log_in user
+          redirect_to forwarding_url || user
+        else
+          flash.now[:danger] = "ログインに失敗しました。1時間後に再度お試しください。"
+          redirect_to root_url
+        end
       else
         message  = "アカウントが登録できませんでした。"
         message += "あなたのメールアドレスに送られたリンクをもう一度確認してください。"
@@ -18,6 +24,8 @@ class SessionsController < ApplicationController
         redirect_to root_url
       end
     else
+      user&.increment!(:login_attempts) # ログイン失敗回数をカウント
+      user&.update(last_attemts_at: Time.zone.now) if user&.login_attempts == 1 # ログイン失敗回数が1回の時に時間を記録
       flash.now[:danger] = "メールアドレスかパスワードが間違っています。"
       render "new", status: :unprocessable_entity
     end
